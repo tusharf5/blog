367d88eafcaebcfa0c6633d5a2cf5d54f77a9af5
Tushar Sharma <ts17995@gmail.com>
2022-09-18 01:46:35 +0530

    feat: add dark mode

--- src/@types/dynamic-class-list.d.ts
+++
@@ -1,5 +0,0 @@
-declare type Value = string | undefined | null | number;
-declare type ValueMore = Value | Record<string, () => boolean>;
-declare type PossibleValues = ValueMore | Array<ValueMore>;
-declare function cl(..._args: PossibleValues[]): string;
-export { cl };
